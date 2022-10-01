626be0f11eb8765d4bbb6021125b3fcb07660bd9
Sai Nayunipati <53490935+sai-nayunipati@users.noreply.github.com>
2022-10-01 15:22:39 -0400

    Update to final version

--- backend.py
+++ backend.py
@@ -1,9 +1,14 @@
 from flask import Flask
+from flask_cors import CORS
 import requests
-import math
-from bs4 import BeautifulSoup
+# import math
+#from bs4 import BeautifulSoup
 
 app = Flask(__name__)
+
+
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 @app.route('/')
 def index():
@@ -13,8 +18,6 @@ def index():
     return 'Hello from Flask!!'
 
 
-# address will be coordinates in the form (41.40338, 2.17403)
-@app.route('/carbon-footprint/<string:user_address>')
 # Calculate the distance based on two coordinates
 def calculate_distance(start, dest):
     """
@@ -29,8 +32,7 @@ def calculate_distance(start, dest):
     response = requests.get(
         f'https://api.radar.io/v1/route/distance?origin={start[0]}, {start[1]}&destination={dest[0]},{dest[1]}&modes=foot,car&units=imperial',
         headers=headers)
-    distance = round(
-        response.json()['routes']['car']['distance']['value'] / 5280, 2)
+    distance = round(response.json()['routes']['car']['distance']['value']/5280, 2)
 
     return distance
 
@@ -48,24 +50,27 @@ def nearest_centerdistance(user_location):
     lst = []
     bos7_distance = calculate_distance(user_location,
                                        fulfillment_centers['BOS7'])
-    bos5_distance = calculate_distance(user_location,
-                                       fulfillment_centers['BOS5'])
+    #bos5_distance = calculate_distance(user_location,
+    #                                   fulfillment_centers['BOS5'])
     cb02_distance = calculate_distance(user_location,
                                        fulfillment_centers['DBO2'])
     lst.append(bos7_distance)
-    lst.append(bos5_distance)
+    #lst.append(bos5_distance)
     lst.append(cb02_distance)
     nearest_center_distance = min(lst)
 
     return nearest_center_distance
+    #print(nearest_center_distance((42.3515055, -71.105615)))
 
 
-def calculate_carbon_footprint(address):
+# address will be coordinates in the form (41.40338, 2.17403)
+@app.route('/carbon-footprint/<latitude>/<longitude>')
+def calculate_carbon_footprint(latitude, longitude):
     """
-  This is the resource the frontend will have access to.
-  """
+    This is the resource the frontend will have access to.
+    """
 
-    shipping_dist = nearest_centerdistance(address)
+    shipping_dist = nearest_centerdistance((42.3490594, -71.1017777))
 
     headers = {
         'Authorization': 'Bearer R012NVSEG7MGQVQXX9BFPVJ19SWN',
@@ -79,6 +84,7 @@ def calculate_carbon_footprint(address):
                              headers=headers,
                              data=data)
 
-    return round(response.json()["co2e"], 2)
+    return str(round(response.json()["co2e"], 2))
+
 
 app.run(host='0.0.0.0', port=81)
